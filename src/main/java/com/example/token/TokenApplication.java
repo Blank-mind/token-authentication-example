207611a435731464a73f67bf1effc9db27aa9cdc
vardlloo <vzardlloo@gmail.com>
2018-08-02 23:36:19 +0800

    :tada: init project

---
+++ src/main/java/com/example/token/TokenApplication.java
@@ -0,0 +1,12 @@
+package com.example.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenApplication.class, args);
+    }
+}
